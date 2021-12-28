c6da93433ea9230bde915871d0df775597b19d69
mangr7743 <ravimangat27@gmail.com>
2021-12-28 18:15:53 -0500

    Created create-item route for making NFT

--- nft-marketplace/pages/index.js
+++ nft-marketplace/pages/index.js
@@ -19,7 +19,7 @@ function Home() {
   }, [])
 
   async function loadNFTs() {
-    const provier = new ethers.providers.JsonRpcProvider()
+    const provider = new ethers.providers.JsonRpcProvider()
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems()
