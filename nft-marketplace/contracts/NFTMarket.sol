08c3938ac74e8d8650bce1dec82f320c6ad76ec2
mangr7743 <ravimangat27@gmail.com>
2021-12-27 19:14:30 -0500

    Setup tests for contracts and minor contract updates

--- nft-marketplace/contracts/NFTMarket.sol
+++ nft-marketplace/contracts/NFTMarket.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFTMarket is ReentrancyGuard {
     using Counters for Counters.Counter;
--- nft-marketplace/hardhat.config.js
+++ nft-marketplace/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
-
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString();
 
 module.exports = {
   networks: {
@@ -7,9 +8,13 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      url: `https://goerli.infura.io/v3/${process.env.projectId}`
+      url: `https://goerli.infura.io/v3/${process.env.projectId}`,
+      accounts: [privateKey]
     },
-    mainnet: {}
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.projectId}`,
+      accounts: [privateKey]
+    }
   },
   solidity: "0.8.4",
 };
